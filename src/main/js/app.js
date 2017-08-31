3464f6ccecd3c7c77e56fba4b5df6d00c9ca7669
ymsong01 <ymsong01@gmail.com>
2017-08-31 11:27:29 -0400

    editing front-end with some font styles 2

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,3 +1,4 @@
+'use strict';
 const React = require('react');
 const ReactDOM = require('react-dom');
 var Client = require('node-rest-client').Client;
@@ -16,21 +17,45 @@ class App extends React.Component {
 	componentDidMount() {
 	    var that = this;
 	    client.get("http://localhost:8080/posts", function (data, response) {
-            console.log("data (buffer)");
-            console.log(data.toString());
-            that.state.posts.push(data.toString());
-            that.setState({posts: that.state.posts});
-            console.log("Posts state:");
-            console.log(that.state.posts);
+	        var i, posts = JSON.parse(data.toString())._embedded.posts;
+	        for (i = 0; i < posts.length; i+=1)
+	            this.state.posts.push(posts[i]);
+
+	        console.log(data.toJSON());
+
+	        console.log(JSON.parse(data.toString()));
+
+
+
+
+	        console.log(response);
+	        console.log(response.entity._embedded);
+
+          that.setState({posts: that.state.posts});
+          console.log("Posts state:");
+          console.log(that.state.posts);
         });
 	}
 
 	render() {
 		return (
-		    <h1>{this.state.posts}</h1>
+		    <Posts listOfPosts={this.state.posts}/>
 		);
 	}
 }
+
+const Posts = ({listOfPosts}) => (
+  <div>
+    {
+      listOfPosts.map(post => (
+        <div class="post">
+          <h1>{post.title}</h1>
+          <p>{post.summary}</p>
+        </div>
+      ))
+    }
+  </div>
+);
 
 ReactDOM.render(
 	<App />,
