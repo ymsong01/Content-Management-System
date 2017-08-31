8ef53ae43d3cb46b22f473ade3940745f7a59e23
ymsong01 <ymsong01@gmail.com>
2017-08-31 11:11:23 -0400

    changing app.js for testing post

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,17 +1,34 @@
 const React = require('react');
-const ReactDOM = require('react-dom')
+const ReactDOM = require('react-dom');
+var Client = require('node-rest-client').Client;
+var client = new Client();
 
 class App extends React.Component {
 
 	constructor(props) {
 		super(props);
+		this.state = {
+		    posts: [],
+		};
 
+	}
+
+	componentDidMount() {
+	    var that = this;
+	    client.get("http://localhost:8080/posts", function (data, response) {
+            console.log("data (buffer)");
+            console.log(data.toString());
+            that.state.posts.push(data.toString());
+            that.setState({posts: that.state.posts});
+            console.log("Posts state:");
+            console.log(that.state.posts);
+        });
 	}
 
 	render() {
 		return (
-			<h1>Hello World!!!</h1>
-		)
+		    <h1>{this.state.posts}</h1>
+		);
 	}
 }
 
